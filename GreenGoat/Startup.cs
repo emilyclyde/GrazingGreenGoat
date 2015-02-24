ae4c66861657b2989df031ece630e7cfa24d0cad
Emily <emily.clyde@yahoo.com>
2015-02-23 20:15:41 -0800

    Rebuild
    Drop down list, Data Migrations & Deployment to Azure

---
+++ GreenGoat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GreenGoat.Startup))]
+namespace GreenGoat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ GreenGoat/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GreenGoat.MvcApplication" Language="C#" %>
