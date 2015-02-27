d87c3855438ee2d18b256ff5bb4b74b41fb81f4f
Emily <emily.clyde@yahoo.com>
2015-02-26 17:56:11 -0800

    Rebuild
    New rebuild

--- GreenGoat/obj/Release/Package/PackageTmp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GreenGoat.MvcApplication" Language="C#" %>
---
+++ GreenGrazingGoat/Controllers/SaleController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GreenGrazingGoat.Controllers
+{
+  public class SaleController : Controller
+  {
+
+    // GET: Sale
+    public ActionResult Index()
+    {
+      ViewBag.MyMessageToUsers = "Hello from Green Grazing Goats!!! This year we are expanding our herd. We have little baby goats! If you would like to purchase a new baby this year please contact us";
+      return View();
+    }
+  }
+}
