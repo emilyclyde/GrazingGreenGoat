a086b46204b85b600b1f0a8cc33910f0baad467d
Emily <emily.clyde@yahoo.com>
2015-03-02 11:25:50 -0800

    Added a customer repository
    Code Change in the Customer Controller

--- GreenGrazingGoat/Controllers/CustomerController.cs
+++ GreenGrazingGoat/Controllers/CustomerController.cs
@@ -13,32 +13,26 @@ namespace GreenGrazingGoat.Controllers
 {
   public class CustomerController : Controller
   {
-    private GreenContext db = new GreenContext();
+
+    private ICustomerRepository repo;
+
+    //called by the MVC framework thrugh the repository
+    public CustomerController()
+    {
+      this.repo = new CustomerRepository(new GreenContext());
+    }
+
+    //called by the unit test this is the fake repository
+    public CustomerController(ICustomerRepository fakeRepo)
+    {
+      this.repo = fakeRepo;
+    }
 
     // GET: Customer
-    public ActionResult Index(string sortOrder, string searchString)
+    public ActionResult Index()
     {
-      ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
 
-      var customers = from c in db.Customers
-                      select c;
-
-      if (!String.IsNullOrEmpty(searchString))
-      {
-        customers = customers.Where(c => c.CustomerLast.Contains(searchString));
-      }
-
-      switch (sortOrder)
-      {
-        case "name_desc":
-          customers = customers.OrderByDescending(c => c.CustomerLast);
-          break;
-        default:
-          customers = customers.OrderBy(c => c.CustomerLast);
-          break;
-      }
-      return View(customers.ToList());
-      //return View(db.Customers.ToList());
+      return View(repo.GetCustomers());
     }
 
     // GET: Customer/Details/5
@@ -48,7 +42,7 @@ namespace GreenGrazingGoat.Controllers
       {
         return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
       }
-      Customer customer = db.Customers.Find(id);
+      Customer customer = repo.GetCustomerByID((int)id);//use caste to solve the int problem
       if (customer == null)
       {
         return HttpNotFound();
@@ -59,6 +53,7 @@ namespace GreenGrazingGoat.Controllers
     // GET: Customer/Create
     public ActionResult Create()
     {
+
       return View();
     }
 
@@ -67,36 +62,18 @@ namespace GreenGrazingGoat.Controllers
     // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
     [HttpPost]
     [ValidateAntiForgeryToken]
-    /* public ActionResult Create([Bind(Include = "CustomerID,CustomerFirst,CustomerLast,CustomerAddress,CustomerEmail")] Customer customer)
-     {
-       if (ModelState.IsValid)
-       {
-         db.Customers.Add(customer);
-         db.SaveChanges();
-         return RedirectToAction("Index");
-       }
-
-       return View(customer);
-     }
-    */
     public ActionResult Create([Bind(Include = "CustomerID,CustomerFirst,CustomerLast,CustomerAddress,CustomerEmail")] Customer customer)
     {
-      try
+      if (ModelState.IsValid)
       {
-        if (ModelState.IsValid)
-        {
-          db.Customers.Add(customer);
-          db.SaveChanges();
-          return RedirectToAction("Index");
-        }
+        repo.InsertCustomer(customer);
+        repo.Save();
+        return RedirectToAction("Index");
       }
-      catch (DataException /* dex */)
-      {
-        //Log the error (uncomment dex variable name and add a line here to write a log.
-        ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
-      }
+
       return View(customer);
     }
+
     // GET: Customer/Edit/5
     public ActionResult Edit(int? id)
     {
@@ -104,7 +81,7 @@ namespace GreenGrazingGoat.Controllers
       {
         return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
       }
-      Customer customer = db.Customers.Find(id);
+      Customer customer = repo.GetCustomerByID((int)id);
       if (customer == null)
       {
         return HttpNotFound();
@@ -115,72 +92,27 @@ namespace GreenGrazingGoat.Controllers
     // POST: Customer/Edit/5
     // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
     // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-    /*[HttpPost]
+    [HttpPost]
     [ValidateAntiForgeryToken]
     public ActionResult Edit([Bind(Include = "CustomerID,CustomerFirst,CustomerLast,CustomerAddress,CustomerEmail")] Customer customer)
     {
       if (ModelState.IsValid)
       {
-        db.Entry(customer).State = EntityState.Modified;
-        db.SaveChanges();
+
+        repo.Save();
         return RedirectToAction("Index");
       }
       return View(customer);
     }
-    */
-    [HttpPost, ActionName("Edit")]
-    [ValidateAntiForgeryToken]
-    public ActionResult EditPost(int? id)
-    {
-      if (id == null)
-      {
-        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-      }
-      var customerToUpdate = db.Customers.Find(id);
-      if (TryUpdateModel(customerToUpdate, "",
-         new string[] { "CustomerID", "CustomerFirst", "CustomerLast", "CustomerAddress", "CustomerEmail" }))
-      {
-        try
-        {
-          db.Entry(customerToUpdate).State = EntityState.Modified;
-          db.SaveChanges();
-
-          return RedirectToAction("Index");
-        }
-        catch (DataException /* dex */)
-        {
-          //Log the error (uncomment dex variable name and add a line here to write a log.
-          ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
-        }
-      }
-      return View(customerToUpdate);
-    }
 
     // GET: Customer/Delete/5
-    /*public ActionResult Delete(int? id)
+    public ActionResult Delete(int? id)
     {
       if (id == null)
       {
         return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
       }
-      Customer customer = db.Customers.Find(id);
-      if (customer == null)
-      {
-        return HttpNotFound();
-      }
-      return View(customer);
-    }*/
-    public ActionResult Delete(int? id, bool? saveChangesError = false)
-    {
-      if (id == null)
-      {
-        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-      }
-      if (saveChangesError.GetValueOrDefault())
-      {
-        ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
-      }
-      Customer customer = db.Customers.Find(id);
+      Customer customer = repo.GetCustomerByID((int)id);
       if (customer == null)
       {
         return HttpNotFound();
@@ -191,34 +123,19 @@ namespace GreenGrazingGoat.Controllers
     // POST: Customer/Delete/5
     [HttpPost, ActionName("Delete")]
     [ValidateAntiForgeryToken]
-    /* public ActionResult DeleteConfirmed(int id)
-     {
-       Customer customer = db.Customers.Find(id);
-       db.Customers.Remove(customer);
-       db.SaveChanges();
-       return RedirectToAction("Index");
-     }*/
-    public ActionResult Delete(int id)
+    public ActionResult DeleteConfirmed(int id)
     {
-      try
-      {
-        Customer customer = db.Customers.Find(id);
-        db.Customers.Remove(customer);
-        db.SaveChanges();
-      }
-      catch (DataException/* dex */)
-      {
-        //Log the error (uncomment dex variable name and add a line here to write a log.
-        return RedirectToAction("Delete", new { id = id, saveChangesError = true });
-      }
+      repo.DeleteCustomer(id);
+      repo.Save();
       return RedirectToAction("Index");
     }
+
 
     protected override void Dispose(bool disposing)
     {
       if (disposing)
       {
-        db.Dispose();
+        repo.Dispose();
       }
       base.Dispose(disposing);
     }
---
+++ GreenGrazingGoat/DAL/ICustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿using GreenGrazingGoat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GreenGrazingGoat.DAL
+{
+  public interface ICustomerRepository : IDisposable 
+  {
+    IEnumerable<Customer> GetCustomers();
+    Customer GetCustomerByID(int customerId);
+    void InsertCustomer(Customer customer);
+    void DeleteCustomer(int customerID);
+    void UpdateCustomer(Customer customer);
+    void Save();
+  }
+}
